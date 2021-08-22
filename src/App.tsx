64281930630564bc5bf890bcc5143e25d27bfbe2
Pablo <pgocev@mail.ru>
2021-08-22 14:18:06 +0300

    added list components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import Card, { CardVariant } from './components/card'
+import Card, { CardVariant } from './components/Card'
+import UserList from './components/UserList'
+import { IUser } from './types/types'
 
 const App = () => {
+  const users: IUser[] = [
+    {id:1, name:'Petro', email:'sss@mail.ru', address: {city: 'Moscow', street: 'lenina', zipcode: '24124312'}},
+    {id:2, name:'Petro', email:'sss@mail.ru', address: {city: 'Moscow', street: 'lenina', zipcode: '24124312'}}
+  ]
   return (
     <div>
       <Card onClick={(num: number)=>console.log('click', num)} height='200px' width='200px' variant={CardVariant.primary}> 
         <button>btn</button>
         <div>descr</div>
       </Card>
+      <UserList users={users}/>
     </div>
   )
 }
